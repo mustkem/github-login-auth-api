21e82f406366c539e177caf58be6c0e872739f1b
Mustkeem <mo.mustkem@gmail.com>
2021-08-26 00:08:54 +0530

    Update cors headers

--- server/index.js
+++ server/index.js
@@ -2,26 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const FormData = require("form-data");
 const fetch = require("node-fetch");
-const { client_id, redirect_uri, client_secret } = require("./config");
-
-const config = require("./config");
+var cors = require("cors");
+const { client_id, client_secret } = require("./config");
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Enabled Access-Control-Allow-Origin", "*" in the header so as to by-pass the CORS error.
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+//   next();
+// });
 
 app.post("/authenticate", (req, res) => {
   const { code } = req.body;
@@ -44,7 +45,6 @@ app.post("/authenticate", (req, res) => {
     .then((paramsString) => {
       let params = new URLSearchParams(paramsString);
       access_token = params.get("access_token");
-
       // Request to return data of a user that has been authenticated
       return fetch(`https://api.github.com/user`, {
         headers: {
